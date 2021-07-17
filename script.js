868b9631dddfdeaf61906287ea0418c47f7c2bf4
André Honorato <gamera103@gmail.com>
2021-07-17 17:03:59 -0300

    Adicionando favicon e comando secreto *-*

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const texto = document.querySelector('.texto')
 let ultimoTexto = terminal.lastElementChild.querySelector('.texto p')
 let ultimaSeta = terminal.lastElementChild.querySelector('.linha ion-icon')
 const teclasProibidas = ['CapsLock', 'Shift', 'Alt', 'Meta', 'ControlAltGraph', 'ContextMenu', 'Control', 'Dead', 'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'Delete', 'End', 'PageDown', 'Insert', 'Home', 'PageUp', 'ScrollLock', 'Pause', 'Tab', 'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12']
+let link = false;
 body.onkeydown = e => {
     if (e.key == 'Backspace') {
         ultimoTexto.innerHTML = ultimoTexto.innerHTML.substr(0, (ultimoTexto.innerHTML.length - 1))
@@ -19,8 +20,15 @@ body.onkeydown = e => {
         ultimaSeta.style.display = 'none'
         ultimaBarra.style.display = 'none'
         ultimoTexto.style.marginRight = '20px'
+        if (ultimoTexto.innerHTML == 'github') {
+            link = true
+        }
         terminal.appendChild(linha)
         ultimoTexto = terminal.lastElementChild.querySelector('.texto p')
+        if (link) {
+            ultimoTexto.innerHTML = "<a href='https://github.com/dedehlol/css-terminal' target='_blank'>https://github.com/dedehlol/css-terminal</a>"
+            link = false
+        }
         ultimaSeta = terminal.lastElementChild.querySelector('.linha ion-icon')
     }
     else {
